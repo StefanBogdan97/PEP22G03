c6e6eb23015504f100d686fd29cbb12b48145f01
Bogdan <bogdan0697@yahoo.com>
2022-05-31 21:32:03 +0300

    my first file

---
+++ module1/modul1.py
@@ -0,0 +1,18 @@
+# print('Hello world\n', 'My Object', end="\n" , sep=' -  abcd -')
+#
+# #print('print')
+#
+# print('###', '# #' , '###',sep='\n')
+
+# print(3 + 3)
+# print("3" +"3")
+# print("3" * 5)
+# print("#" * 11)
+# print("#", " " * 7, "#")
+# print("#" * 11)
+
+my_value = 'My object'
+my_value = 15
+my_value2 = 10
+
+print(my_value * my_value2)
