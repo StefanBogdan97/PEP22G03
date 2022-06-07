86402ff41a9faebf251e74a4b3a76bea392188c2
Bogdan <bogdan0697@yahoo.com>
2022-06-07 13:38:19 +0300

    my first file

---
+++ Homework/Homework.py
@@ -0,0 +1,39 @@
+# #1
+# parola ='Passme1n'
+#
+# ParolaSistem=(input('ParolaSistem:'))
+#
+# ParolaSistem == parola
+# print(parola == ParolaSistem)
+
+#2
+
+# Nume=(input('NumeUtilizator:'))
+# Nume2=(input('NumeUtilizator:'))
+#
+# print('Numarul de caractere al primului nume:',len(Nume))
+#
+# print('Similaritatea celor doi utilizatori:',Nume == Nume2)
+# print('Primul utilizator contine mai multe caractere decat al doilea:',len(Nume)>len(Nume2))
+# print('Initiala primului nume este:',Nume[0])
+# print('Numele primului utilizator scris invers este:',Nume[::-1])
+
+#3
+
+# Nume=(input('Numele de referinta:'))
+# Nume2=(input('Introduceti numele:'))
+# Numar=int(input('Selectati orice cifra:'))
+# print('Numele de referinta este acelasi cu numele ales:', Nume == Nume2)
+# print('Numarul caracterelor numelui de referinta este mai mare decat cel ales:',len(Nume)>len(Nume2))
+# print('Initiala numelui de referinta este:',Nume[0])
+# print('Numele de referinta scris invers este:',Nume[::-1])
+# print(Nume*Numar)
+
+#4
+#
+# Nume ='Ananas'
+# print(Nume[0],Nume[1],Nume[2],Nume[3],Nume[4],Nume[5],sep='\n')
+# print(Nume[0:3],Nume[3:6],sep='\n')
+# print(Nume[0:2],Nume[2:5],Nume[4:6],sep=':')
+# print(Nume[0:3],Nume[3:5],Nume[5:6],sep='_')
+# print(Nume[1:3]*8,'batman')
